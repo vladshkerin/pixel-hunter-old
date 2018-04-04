1c1598f6fd3db12cfae622dceb911bb095be78c1
Vladimir Shkerin <vladshkerin@gmail.com>
2018-04-04 20:43:29 +0300

    #4.3: created modules from templates

---
+++ js/templates/intro.js
@@ -0,0 +1,9 @@
+import {getElementFromTemplate} from '../createDOM';
+
+const template = `<div id="intro" class="intro">
+  <h1 class="intro__asterisk">*</h1>
+  <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf
+    Sparnaay.</p>
+</div>`;
+
+export default getElementFromTemplate(template);
