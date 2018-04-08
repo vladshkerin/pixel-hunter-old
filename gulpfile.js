06f9ff3cc2a8fef4f07c3ae35d96e5269407ca97
Vladimir Shkerin <vladshkerin@gmail.com>
2018-04-08 09:48:03 +0300

    #5: testing and configuration

--- gulpfile.js
+++ gulpfile.js
@@ -107,3 +107,13 @@ gulp.task('assemble', ['clean'], function () {
 gulp.task('build', ['assemble'], function () {
   gulp.start('imagemin');
 });
+
+const mocha = require('gulp-mocha'); // Добавим установленный gulp-mocha плагин
+gulp.task('test', function () {
+  return gulp
+    .src(['js/**/*.test.js'], {read: false})
+    .pipe(mocha({
+      compilers: ['js:babel-register'], // Включим поддержку "import/export" в Mocha тестах
+      reporter: 'spec' // Вид в котором я хочу отображать результаты тестирования
+    }));
+});
